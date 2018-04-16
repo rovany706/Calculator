4250f47c78b5a437a01938909bd2ef72cd1c1f65
SvetlanaRonzina <swet.ronzina@yandex.ru>
2018-04-16 21:18:18 +0500

    Добавлен класс Calc, добавлены два события. Изменена архитектура

---
+++ Calculator/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+    public  static class Calc
+    {
+        public static string DistanceBetweenDates(DateTime dateTimePicker1, DateTime dateTimePicker2)
+        {
+        string res = null;
+            return res;
+        }
+        public static string DateСalculation(DateTime dateTimePicker3, string textBox1, string textBox2, bool radioButton1)
+        {
+            string res = null;
+            return res;
+
+        }
+    }
+}
